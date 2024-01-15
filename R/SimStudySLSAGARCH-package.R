f86c56a08caaf8f37773939e37ae79f0ce74f1f2
sjvrensburg <stefanj@mandela.ac.za>
2024-01-15 14:12:15 +0200

    Package skeleton

---
+++ R/SimStudySLSAGARCH-package.R
@@ -0,0 +1,7 @@
+## usethis namespace: start
+#' @importFrom Rcpp sourceCpp
+#' @import RcppEigen
+#' @import SLSAGARCH
+#' @useDynLib SimStudySLSAGARCH, .registration = TRUE
+## usethis namespace: end
+NULL
