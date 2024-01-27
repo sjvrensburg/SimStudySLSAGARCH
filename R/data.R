ad0d94cb1bc118199b0da367c5e32192020afe0f
sjvrensburg <stefanj@mandela.ac.za>
2024-01-27 15:59:09 +0200

    Simulated data for study

--- R/data.R
+++ R/data.R
@@ -18,3 +18,93 @@
 #' @format A 9x6 data frame with a column for each parameter.
 #' @source Own calculations.
 "parameters"
+
+#' Scenario 1
+#'
+#' Data for the case where \eqn{\lambda = 0,} \eqn{\rho = 0} and
+#' \eqn{n \in \{250, 500, 750, 1000, 1250, 2500 \},} i.e.,
+#' the 1st row of `parameters`.
+#'
+#' @format A list of matrices that contain the simulated observations.
+#' @source Own calculations.
+"scenario_01"
+
+#' Scenario 2
+#'
+#' Data for the case where \eqn{\lambda = 0.5,} \eqn{\rho = 0} and
+#' \eqn{n \in \{250, 500, 750, 1000, 1250, 2500 \},} i.e.,
+#' the 2nd row of `parameters`.
+#'
+#' @format A list of matrices that contain the simulated observations.
+#' @source Own calculations.
+"scenario_02"
+
+#' Scenario 3
+#'
+#' Data for the case where \eqn{\lambda = 1,} \eqn{\rho = 0} and
+#' \eqn{n \in \{250, 500, 750, 1000, 1250, 2500 \},} i.e.,
+#' the 3rd row of `parameters`.
+#'
+#' @format A list of matrices that contain the simulated observations.
+#' @source Own calculations.
+"scenario_03"
+
+#' Scenario 4
+#'
+#' Data for the case where \eqn{\lambda = 0,} \eqn{\rho = 0.25} and
+#' \eqn{n \in \{250, 500, 750, 1000, 1250, 2500 \},} i.e.,
+#' the 4th row of `parameters`.
+#'
+#' @format A list of matrices that contain the simulated observations.
+#' @source Own calculations.
+"scenario_04"
+
+#' Scenario 5
+#'
+#' Data for the case where \eqn{\lambda = 0.5,} \eqn{\rho = 0.25} and
+#' \eqn{n \in \{250, 500, 750, 1000, 1250, 2500 \},} i.e.,
+#' the 5th row of `parameters`.
+#'
+#' @format A list of matrices that contain the simulated observations.
+#' @source Own calculations.
+"scenario_05"
+
+#' Scenario 6
+#'
+#' Data for the case where \eqn{\lambda = 1,} \eqn{\rho = 0.25} and
+#' \eqn{n \in \{250, 500, 750, 1000, 1250, 2500 \},} i.e.,
+#' the 6th row of `parameters`.
+#'
+#' @format A list of matrices that contain the simulated observations.
+#' @source Own calculations.
+"scenario_06"
+
+#' Scenario 7
+#'
+#' Data for the case where \eqn{\lambda = 0,} \eqn{\rho = 0.75} and
+#' \eqn{n \in \{250, 500, 750, 1000, 1250, 2500 \},} i.e.,
+#' the 7th row of `parameters`.
+#'
+#' @format A list of matrices that contain the simulated observations.
+#' @source Own calculations.
+"scenario_07"
+
+#' Scenario 8
+#'
+#' Data for the case where \eqn{\lambda = 0.5,} \eqn{\rho = 0.75} and
+#' \eqn{n \in \{250, 500, 750, 1000, 1250, 2500 \},} i.e.,
+#' the 8th row of `parameters`.
+#'
+#' @format A list of matrices that contain the simulated observations.
+#' @source Own calculations.
+"scenario_08"
+
+#' Scenario 9
+#'
+#' Data for the case where \eqn{\lambda = 1,} \eqn{\rho = 0.75} and
+#' \eqn{n \in \{250, 500, 750, 1000, 1250, 2500 \},} i.e.,
+#' the 9th row of `parameters`.
+#'
+#' @format A list of matrices that contain the simulated observations.
+#' @source Own calculations.
+"scenario_09"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -15,3 +15,64 @@ P <- apply(parameters, 1, function(x) {
 })
 parameters[, 2] <- 1 - P
 usethis::use_data(parameters, overwrite = TRUE)
+
+# Datasets
+seed <- 20240127L
+R <- 10000
+n <- c(250, 500, 750, 1000, 1250, 2500)
+
+scenario_01 <- lapply(n, function(x) {
+  par0 <- unlist(parameters[1, ])
+  dgp(spec = sls_sech_spec, param = par0,
+      n = x, R = R, seed = seed, nthreads = 14L)
+})
+scenario_02 <- lapply(n, function(x) {
+  par0 <- unlist(parameters[2, ])
+  dgp(spec = sls_sech_spec, param = par0,
+      n = x, R = R, seed = seed, nthreads = 14L)
+})
+scenario_03 <- lapply(n, function(x) {
+  par0 <- unlist(parameters[3, ])
+  dgp(spec = sls_sech_spec, param = par0,
+      n = x, R = R, seed = seed, nthreads = 14L)
+})
+scenario_04 <- lapply(n, function(x) {
+  par0 <- unlist(parameters[4, ])
+  dgp(spec = sls_sech_spec, param = par0,
+      n = x, R = R, seed = seed, nthreads = 14L)
+})
+scenario_05 <- lapply(n, function(x) {
+  par0 <- unlist(parameters[5, ])
+  dgp(spec = sls_sech_spec, param = par0,
+      n = x, R = R, seed = seed, nthreads = 14L)
+})
+scenario_06 <- lapply(n, function(x) {
+  par0 <- unlist(parameters[6, ])
+  dgp(spec = sls_sech_spec, param = par0,
+      n = x, R = R, seed = seed, nthreads = 14L)
+})
+scenario_07 <- lapply(n, function(x) {
+  par0 <- unlist(parameters[7, ])
+  dgp(spec = sls_sech_spec, param = par0,
+      n = x, R = R, seed = seed, nthreads = 14L)
+})
+scenario_08 <- lapply(n, function(x) {
+  par0 <- unlist(parameters[8, ])
+  dgp(spec = sls_sech_spec, param = par0,
+      n = x, R = R, seed = seed, nthreads = 14L)
+})
+scenario_09 <- lapply(n, function(x) {
+  par0 <- unlist(parameters[9, ])
+  dgp(spec = sls_sech_spec, param = par0,
+      n = x, R = R, seed = seed, nthreads = 14L)
+})
+
+usethis::use_data(scenario_01, overwrite = TRUE, compress = "gzip")
+usethis::use_data(scenario_02, overwrite = TRUE, compress = "gzip")
+usethis::use_data(scenario_03, overwrite = TRUE, compress = "gzip")
+usethis::use_data(scenario_04, overwrite = TRUE, compress = "gzip")
+usethis::use_data(scenario_05, overwrite = TRUE, compress = "gzip")
+usethis::use_data(scenario_06, overwrite = TRUE, compress = "gzip")
+usethis::use_data(scenario_07, overwrite = TRUE, compress = "gzip")
+usethis::use_data(scenario_08, overwrite = TRUE, compress = "gzip")
+usethis::use_data(scenario_09, overwrite = TRUE, compress = "gzip")
